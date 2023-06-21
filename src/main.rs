8578040c35b331a69e7506a68641ca734b14773d
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2023-06-21 00:29:11 -0500

    change prime bits

--- src/main.rs
+++ src/main.rs
@@ -22,14 +22,14 @@ fn main() {
     // The length of the prime numbers generated, in bits.
     // we should change that often, just like K in rX. maybe figure ut a way to generate that from some blocknumbers, e.g., use
     // randomx -like K approach and then take the last 4 digits of target block
-    let num_bits: u16 = 1024 * 8;
+    let num_bits: u16 = 2024; // * 8;
 
     // here we generate a data blog, hash it, sign the hash  and use it
     // as the challenge for the VDF
     // heck, we might also consider add or insert the signature, maybe of a nonce, to the data that way we could use
     // the same data for multiple providers ... something to playbwith.
     // ideally, the data blob get's created with an updated nonce after every vdf output, e.g., signed hash of vdf out plus previous nonce
-    let data_size = 1024; // * 1024 * 1024 * * 1024 * 16 // 16 GB
+    let data_size = 512; // 1024 * 1024 * 1024 * * 1024 * 16 // 16 GB
 
     // need nonce. maybe like K also
     let nonce = b"iamanonce"; // could use the hash of output from previous run and the submission batch requires order.
@@ -41,7 +41,7 @@ fn main() {
     let challenge = &keypair.sign(&data_hash).to_bytes();
 
     // let challenge = b"iamasignednonce";
-    let difficulty = 10000; // 1_000_000;
+    let difficulty = 1_000_000; // 1_000_000;
 
     // An instance of the VDF.  Instances can be used arbitrarily many times.
     let start_prover = Instant::now();
@@ -63,10 +63,11 @@ fn main() {
     let valid_proof = pietrzak_vdf.verify(challenge, difficulty, &result).is_ok();
     let verifier_duration = start_verifier.elapsed();
 
-    println!("init vdf duration: {} seconds", init_vdf_duration.as_secs());
-    println!("time to verify: {} seconds", verifier_duration.as_secs());
+    // println!("init vdf duration: {} seconds", init_vdf_duration.as_secs());
+    println!("time to verify: {} millis", verifier_duration.as_millis());
     println!("this is a valid proof: {}", valid_proof);
 
+    /*
     // bad prime bits -- cheater wants a lower number -- no go
     // ideally that's independently verifiable. eg.., derive from blocknumber
     let pietrzak_vdf = PietrzakVDFParams(num_bits - 1).new();
@@ -76,4 +77,5 @@ fn main() {
         true
     };
     println!("this is a not a valid proof: {}", invalid_proof);
+    */
 }
